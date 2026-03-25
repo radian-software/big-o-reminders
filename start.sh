1f6cd8721a1c95592d033df1f50ddea8bd2424f9
Radon Rosborough <radon@intuitiveexplanations.com>
2026-03-24 19:57:52 -0700

    Extract from private dotfiles

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ -f "$HOME/.profile" ]]; then
+    . "$HOME/.profile"
+fi
+
+set -euo pipefail
+
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
+logfmt='%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+cd "$(dirname "$0")"
+poetry sync
+poetry run gunicorn                          \
+       -b 0.0.0.0:"${BIG_O_REMINDERS_PORT}"  \
+       --enable-stdio-inheritance            \
+       --access-logfile -                    \
+       --access-logformat "${logfmt}"        \
+       app:app
